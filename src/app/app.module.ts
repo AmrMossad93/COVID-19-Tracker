efe4aa0d4500f3a59cfeb5b35a31ab7352873beb
AmrMossad93 <eng.amrmossad93@gmail.com>
2020-04-12 22:51:12 +0200

    FrameWork Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './Pages/home-page/home-page.component';
+import {CardComponent} from './Widgets/card/card.component';
+import {NavbarComponent} from './LayOuts/navbar/navbar.component';
+import {FooterComponent} from './LayOuts/footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './angular-material.module';
+import {PrimeNgModule} from './prime-ng.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CardComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule,
+    PrimeNgModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
